f5ea8257e4654028ffe85e8b1507f0a1fc63de10
Puneet Goyal <pungoyal@gmail.com>
2009-10-16 14:10:48 +0530

    adding the name validation

--- test/unit/movie_test.rb
+++ test/unit/movie_test.rb
@@ -5,4 +5,12 @@ class MovieTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test "name is mandatory" do
+    movie = Movie.new
+    assert !movie.valid?
+
+    movie.name = "Up"
+    assert movie.valid?
+  end
 end
